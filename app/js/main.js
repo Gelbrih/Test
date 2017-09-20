1f98ddec2816a5df82216146f25da3bb45da16d2
Gelbrih <dreamlikeforest@gmail.com>
2017-09-20 14:19:26 +0300

    task #2

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,3 @@
-var elem = document.querySelector(".wrap");
-var elemText = elem.textContent.trim();
-
-function firstLetterToUpperCase() {
-	elem.textContent = elemText.charAt(0).toUpperCase() + elemText.slice(1); 
-};
-
-function allFirstLettersToUpperCase() {
-	var wordsArr = elemText.split(" ");
-	for(var i = 0; i<wordsArr.length; i++) {
-		wordsArr[i] = wordsArr[i].charAt(0).toUpperCase() + wordsArr[i].slice(1);
-	}
-	elem.textContent = wordsArr.join(" ");
-};
+(function arg() {
+	console.log(isArray(arguments) )
+})();
