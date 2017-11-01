f8f4c1fb5be5d184c22ad1d7ecd398809efbb95b
Gelbrih <dreamlikeforest@gmail.com>
2017-11-01 18:18:05 +0300

    Functional inheritance. Task #1

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,36 @@
-(function arg() {
-	console.log(isArray(arguments) )
-})();
+ function Machine(power) {
+ 	this._enabled = false;
+
+ 	this.enable = function() {
+ 		this._enabled = true;
+ 	};
+
+ 	this.disable = function() {
+ 		this._enabled = false;
+ 	};
+ }
+
+ function CoffeeMachine(power) {
+ 	Machine.apply(this, arguments);
+
+ 	var waterAmount = 0;
+
+ 	this.setWaterAmount = function(amount) {
+ 		waterAmount = amount;
+ 	};
+
+ 	function onReady() {
+ 		alert('Кофе готово!');
+ 	}
+
+ 	this.run = function() {
+ 		if(!this._enabled) throw new Error('Ошибка! Кофеварка не включена.');
+ 		else setTimeout(onReady, 1000);
+ 	};
+ 	
+
+ };
+
+var coffeeMachine = new CoffeeMachine(10000);
+coffeeMachine.enable();
+coffeeMachine.run(); // ошибка, кофеварка выключена!
