78f6f5a7aabd92d8ea1288dfde8f2cfaecde2bd1
Gelbrih <dreamlikeforest@gmail.com>
2018-11-19 16:56:08 +0300

    codewars task. extract the domain name from a URL

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,9 @@
-'use strict'
-
-function expandedForm(num) {
-	let str = '' + num;
-	let result = '';
-
-	if(num <= 0) return '' + num;
-
-	for(let i = 0; i < str.length; i++) {
-		if(str[i] == 0) continue;
-		
-		result += str[i] + '0'.repeat( str.length - 1 - i ) + '+';
-	}
-
-	return result.slice(0, result.length - 1)
+function domainName(str) {
+	
+	return str.match(/((https?\:\/\/)?(www\.)?)([a-zа-я0-9\-]+)?/i)[4];
 }
 
-console.log(expandedForm(-10));
-console.log(expandedForm(0));
-console.log(expandedForm(3));
-console.log(expandedForm(12)); // Should return '10 + 2'
-console.log(expandedForm(42)); // Should return '40 + 2'
-console.log(expandedForm(70304)); // Should return '70000 + 300 + 4'
-console.log(expandedForm(120470304));
+console.log( domainName("http://github.com/carbonfive/raygun") );// == "github" 
+console.log( domainName("http://www.zombie-bites.com") );// == "zombie-bites"
+console.log( domainName("www.хакер.com") );// == "хакер"
+console.log( domainName("https://www.cnet.com") );// == "cnet"
