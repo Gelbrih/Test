fba3cf48d8185c12cd792612cdfb16d4a3f1020d
Gelbrih <dreamlikeforest@gmail.com>
2018-11-20 15:00:59 +0300

    add bla-bla

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ function anagrams(word, words) {
 	return words.filter( (item, i) => 
 		item.split('').sort().join('') == word.split('').sort().join('') );
 }
-
+//bla-bla
 console.log( anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) );// => ['aabb', 'bbaa']
 console.log( anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) );// => ['carer', 'racer']
 console.log( anagrams('laser', ['lazing', 'lazy',  'lacer']) );// => []
